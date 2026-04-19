3a63ef4650d0e1f8a9a057642ea4886de2ca2f80
Kamil Makarowski <kamil.makarowski@outlook.com>
2026-04-19 14:48:18 +0200

    feat: add IsHoliday method

--- WorkCalendar.Library/GitCwiczenia.cs
+++ WorkCalendar.Library/GitCwiczenia.cs
@@ -5,5 +5,14 @@ namespace WorkCalendar.Library
     public class CalendarHelper
     {
         public static int MaxDaysAhead = 30;
+
+        /// <summary>
+        /// Checks if given date is a holiday (placeholder).
+        /// </summary>
+        public static bool IsHoliday(DateTime date)
+        {
+            // TODO: Integrate with actual holiday calendar
+            return false;
+        }
     }
 }
