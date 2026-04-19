9ba8bb325fa057107a5c6cb52dcd3dd6ef43184a
Kamil Makarowski <kamil.makarowski@outlook.com>
2026-04-19 14:48:45 +0200

    feat: WIP new reporting feature

--- WorkCalendar.Library/GitCwiczenia.cs
+++ WorkCalendar.Library/GitCwiczenia.cs
@@ -5,6 +5,12 @@ namespace WorkCalendar.Library
     public class CalendarHelper
     {
         public static int MaxDaysAhead = 30;
+
+        // WIP: New reporting feature coming
+        public static void GenerateReport()
+        {
+            // TODO: Implement full reporting engine
+            throw new NotImplementedException();
+        }
     }
 }
-// Placeholder for future enhancements
