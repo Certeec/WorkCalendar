129a40a1257a0f344cb5939d99293e3e4a46b1c9
Certeec <certeec@gmail.com>
2026-04-19 21:08:04 +0200

    zad1

--- WorkCalendar.Library/GitCwiczenia.cs
+++ WorkCalendar.Library/GitCwiczenia.cs
@@ -5,6 +5,12 @@ namespace WorkCalendar.Library
     public class CalendarHelper
     {
         public static int MaxDaysAhead = 30;
+
+
+        public int GetWorkinDaysCount()
+        {
+            return 0;
+        }
     }
 }
 // Placeholder for future enhancements
