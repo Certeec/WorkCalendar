651eddae1e433b0864fbf849c263e553180dc7ba
Kamil Makarowski <kamil.makarowski@outlook.com>
2026-04-19 14:47:42 +0200

    WIP: add CalculateWorkingHours method (incomplete)

--- WorkCalendar.Library/GitCwiczenia.cs
+++ WorkCalendar.Library/GitCwiczenia.cs
@@ -5,5 +5,16 @@ namespace WorkCalendar.Library
     public class CalendarHelper
     {
         public static int MaxDaysAhead = 30;
+
+        /// <summary>
+        /// Work in progress: calculates total hours in working days.
+        /// </summary>
+        public static double CalculateWorkingHours(DateTime start, DateTime end)
+        {
+            // TODO: Implement hour calculation
+            // TODO: Add support for custom work hours
+            // TODO: Test with different timezones
+            return 0;
+        }
     }
 }
